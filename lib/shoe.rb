3f7578745fc130f0b7f5fd6c5f073cc464b1f921
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-18 12:31:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
 
-  attr_accessor :brand, :size, :material, :color, :conditon 
+  attr_accessor :brand, :size, :material, :color, :condition 
 
 def initialize(brand)
 @brand = brand
